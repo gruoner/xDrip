168e97f4ec6c440f6cf41b3840f5902883dc9ce3
gruoner <Weich1ei!>
2022-08-01 17:04:49 +0200

    Farben wieder richtiggestellt - sind durch den Merge verloren gegangen

--- app/src/main/java/com/eveningoutpost/dexdrip/BestGlucose.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/BestGlucose.java
@@ -114,11 +114,14 @@ public class BestGlucose {
             if (isStale()) wholeSpan(ret, new StrikethroughSpan());
             if (color) {
                 if (isLow()) {
-                    wholeSpan(ret, new ForegroundColorSpan(getCol(ColorCache.X.color_low_bg_values)));
+                    wholeSpan(ret, new BackgroundColorSpan(getCol(ColorCache.X.color_low_bg_values)));
+                    wholeSpan(ret, new ForegroundColorSpan(Color.parseColor("#ffffff")));
                 } else if (isHigh()) {
-                    wholeSpan(ret, new ForegroundColorSpan(getCol(ColorCache.X.color_high_bg_values)));
+                    wholeSpan(ret, new BackgroundColorSpan(getCol(ColorCache.X.color_high_bg_values)));
+                    wholeSpan(ret, new ForegroundColorSpan(Color.parseColor("#0909C3")));
                 } else {
-                    wholeSpan(ret, new ForegroundColorSpan(getCol(ColorCache.X.color_inrange_bg_values)));
+//                    wholeSpan(ret, new BackgroundColorSpan(getCol(ColorCache.X.color_inrange_bg_values)));
+                    wholeSpan(ret, new ForegroundColorSpan(Color.parseColor("#000000")));
                 }
             }
             return ret;
