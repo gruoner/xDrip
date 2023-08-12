400ab9b46036d0649ffc6a4a3494604ca246e2b4
gruoner <Weich1ei!>
2023-08-12 21:18:14 +0200

    Mergebug

--- app/src/main/java/com/eveningoutpost/dexdrip/Home.java
+++ app/src/main/java/com/eveningoutpost/dexdrip/Home.java
@@ -3390,7 +3390,9 @@ public class Home extends ActivityWithMenu implements ActivityCompat.OnRequestPe
                             0);
                     return null;
                 } else {
-                    return DatabaseUtil.saveSql(getBaseContext());
+                    String f =  DatabaseUtil.saveSql(getBaseContext());
+                    ConfigureImportExport.dispatchAdditionalExports(f, true, false);
+                    return f;
                 }
 
             }
